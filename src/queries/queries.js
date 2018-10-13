6e6f78739ab66b6b1b2e917e3c14a5715c56303b
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-13 20:13:24 +0530

    queries modified and added

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -12,9 +12,9 @@ export const GET_USER = gql`
   }
 `;
 
-export const GET_PINNED_REPOSITORIES = gql`
+export const GET_TABS_DATA = gql`
   query($User: String!) {
-    user(login: $User) {
+    pinnedRepositories: user(login: $User) {
       pinnedRepositories(first: 6) {
         edges {
           node {
@@ -26,6 +26,138 @@ export const GET_PINNED_REPOSITORIES = gql`
             }
           }
         }
+      }
+    }
+
+    repositories: user(login: $User) {
+      repositories(last: 60) {
+        edges {
+          node {
+            id
+            name
+            description
+            updatedAt
+            licenseInfo {
+              name
+            }
+            stargazers {
+              totalCount
+            }
+            primaryLanguage {
+              name
+            }
+          }
+        }
+      }
+    }
+
+    starredRepositories: user(login: $User) {
+      starredRepositories(last: 20) {
+        edges {
+          node {
+            id
+            name
+            description
+            primaryLanguage {
+              name
+            }
+            stargazers {
+              totalCount
+            }
+            forkCount
+            updatedAt
+            viewerHasStarred
+          }
+        }
+      }
+    }
+
+    followers: user(login: $User) {
+      followers(first: 10) {
+        totalCount
+        pageInfo {
+          endCursor
+          startCursor
+          hasPreviousPage
+          hasNextPage
+        }
+        edges {
+          node {
+            id
+            name
+            login
+            avatarUrl
+            viewerIsFollowing
+          }
+        }
+      }
+    }
+
+    following: user(login: $User) {
+      following(first: 10) {
+        totalCount
+        pageInfo {
+          endCursor
+          startCursor
+          hasPreviousPage
+          hasNextPage
+        }
+        edges {
+          node {
+            id
+            name
+            login
+            avatarUrl
+            viewerIsFollowing
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_ORGANIZATION = gql`
+  query($Organization: String!) {
+    organization(login: $Organization) {
+      name
+      url
+    }
+  }
+`;
+
+export const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query($Organization: String!) {
+    organization(login: $Organization) {
+      repositories(first: 20) {
+        edges {
+          node {
+            id
+            name
+            url
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+export const UNSTAR_REPOSITORY = gql`
+  mutation($repositoryId: ID!) {
+    removeStar(input: { starrableId: $repositoryId }) {
+      starrable {
+        viewerHasStarred
       }
     }
   }
