0496f478e16d3b969a3da72e259422ad91c90313
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-14 23:24:00 +0530

    router implementation at root

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -1,37 +1,26 @@
 import React from 'react';
-import { CustomText } from '../styled-components/StyledComponents';
-import DynamicContent from '../DynamicContent/DynamicContent';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../Home/Home';
 
 const Main = props => {
   const { avatarUrl, bio, location, name, websiteUrl } = props;
+  const homeProps = {
+    ...avatarUrl,
+    ...bio,
+    ...location,
+    ...name,
+    ...websiteUrl
+  };
 
   return (
     <main className="Container flex justify-center pa3 tc">
-      <div className="static-content mr3 w5 mw5 tl">
-        <img className="br2" src={avatarUrl} alt={name} />
-        <CustomText
-          className="dark-gray"
-          padding="0.5rem 0"
-          fontSize="2rem"
-          fontWeight="bold"
-        >
-          {name}
-        </CustomText>
-        <CustomText padding="0.5rem 0" fontSize="1rem">
-          <b className="dark-gray">Bio: </b>
-          {bio}
-        </CustomText>
-        <CustomText padding="0.5rem 0" fontSize="1rem">
-          <b className="dark-gray">Location: </b>
-          {location}
-        </CustomText>
-        <CustomText padding="0.5rem 0" fontSize="1rem">
-          <b className="dark-gray">Website: </b>
-          <a href={websiteUrl}>{websiteUrl}</a>
-        </CustomText>
-      </div>
-
-      <DynamicContent />
+      <Switch>
+        {/* <Route path="/" exact strict component={Home}> */}
+        <Route path="/" exact strict>
+          {() => <Home {...homeProps} />}
+        </Route>
+      </Switch>
     </main>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
@@ -43,7 +44,9 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware()));
 const app = (
   <ApolloProvider client={client}>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </ApolloProvider>
 );
