49ae9149e0c5de91727abe8a1b7b83645534780b
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-15 22:34:42 +0530

    Apoolo Client Query removed from App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'tachyons';
 
-import { Query } from 'react-apollo';
-import { GET_USER } from '../queries/queries';
+// import { Query } from 'react-apollo';
+// import { GET_USER } from '../queries/queries';
 
 import Header from './Header/Header';
 import Main from './Main/Main';
 import Footer from './Footer/Footer';
 
-const User = 'kapil5harma';
+// const User = 'kapil5harma';
 
-const App = () => (
-  <Query query={GET_USER} variables={{ User }}>
-    {({ data: { user }, loading }) => {
-      return (
-        <div className="App">
-          <Header />
-          <Main {...user} loading={loading} />
-          <Footer />
-        </div>
-      );
-    }}
-  </Query>
-);
+class App extends Component {
+  // state = {
+  //   user: null,
+  //   loading: false
+  // };
+
+  // componentDidMount = () => {
+  //   console.log('componentDidMount: ');
+  //   return (
+  //     <Query query={GET_USER} variables={{ User }}>
+  //       {res => {
+  //         console.log('return: ');
+  //         console.log('res: ', res);
+  //         const {
+  //           data: { user },
+  //           loading
+  //         } = res;
+  //         this.setState({ user: user, loading: loading });
+  //       }}
+  //     </Query>
+  //   );
+  // };
+
+  // componentDidUpdate = () => {
+  //   console.log('componentDidUpdate: ');
+  //   return (
+  //     <Query query={GET_USER} variables={{ User }}>
+  //       {res => {
+  //         console.log('res: ', res);
+  //         const {
+  //           data: { user },
+  //           loading
+  //         } = res;
+  //         this.setState({ user: user, loading: loading });
+  //       }}
+  //     </Query>
+  //   );
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default App;
