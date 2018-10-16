d1fc5f1f7620f9ccd4d82263679f29a990462527
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-17 00:30:16 +0530

    Watch/Unwatrch button rendered conditionally

--- src/App/RepositoryDetails/RepositoryDetails.js
+++ src/App/RepositoryDetails/RepositoryDetails.js
@@ -5,6 +5,8 @@ import { GET_REPO_DATA } from '../../queries/queries';
 import { Query } from 'react-apollo';
 import Unstar from '../common/Unstar';
 import Star from '../common/Star';
+import Unwatch from '../common/Unwatch';
+import Watch from '../common/Watch';
 
 const User = 'kapil5harma';
 const RepoName = 'Portfolio';
@@ -21,7 +23,7 @@ class RepositoryDetails extends Component {
     fetch(`${baseURL}${apiEndpointToGetContent}`)
       .then(response => response.json())
       .then(data => {
-        console.log('Repo Contents: ', data);
+        // console.log('Repo Contents: ', data);
         this.setState({ repoContents: data });
       })
       .catch(err => {
@@ -41,8 +43,8 @@ class RepositoryDetails extends Component {
           if (loading) {
             return null;
           }
-          console.log('user: ', user);
-          console.log('repository: ', repository);
+          // console.log('user: ', user);
+          // console.log('repository: ', repository);
 
           return (
             <div className="RepositoryDetails flex-column">
@@ -85,22 +87,11 @@ class RepositoryDetails extends Component {
                   </span>
                 </div>
                 <div className="buttons flex h-100">
-                  <span className="black flex ba b--gray bg-light-gray hover-bg-moon-gray pointer br2 pa2 h-100 mh3">
-                    <svg
-                      className="octicon octicon-eye v-align-text-bottom"
-                      viewBox="0 0 16 16"
-                      version="1.1"
-                      width="16"
-                      height="16"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"
-                      />
-                    </svg>
-                    <span className="ml1">{1 ? 'Unwatch' : 'Watch'}</span>
-                  </span>
+                  {repository.viewerSubscription === 'SUBSCRIBED' ? (
+                    <Unwatch repositoryId={repository.id} />
+                  ) : repository.viewerSubscription === 'UNSUBSCRIBED' ? (
+                    <Watch repositoryId={repository.id} />
+                  ) : null}
 
                   {repository.viewerHasStarred ? (
                     <Unstar repositoryId={repository.id} />
