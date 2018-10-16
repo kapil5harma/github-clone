357fd08a90e26ad9e4c34763c5326c88f2e2c852
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-16 23:39:10 +0530

    Slight modifications in RepositoryDetails -- incorporated Star/Unstar functionality

--- src/App/RepositoryDetails/RepositoryDetails.js
+++ src/App/RepositoryDetails/RepositoryDetails.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 import { GET_REPO_DATA } from '../../queries/queries';
 import { Query } from 'react-apollo';
+import Unstar from '../common/Unstar';
+import Star from '../common/Star';
 
 const User = 'kapil5harma';
 const RepoName = 'Portfolio';
@@ -99,23 +101,13 @@ class RepositoryDetails extends Component {
                     </svg>
                     <span className="ml1">{1 ? 'Unwatch' : 'Watch'}</span>
                   </span>
-                  <span className="black flex ba b--gray bg-light-gray hover-bg-moon-gray pointer br2 pa2 h-100 mh3">
-                    <svg
-                      aria-label="star"
-                      className="octicon octicon-star"
-                      viewBox="0 0 14 16"
-                      version="1.1"
-                      width="14"
-                      height="16"
-                      role="img"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
-                      />
-                    </svg>
-                    <span className="ml1">{1 ? 'Unstar' : 'Star'}</span>
-                  </span>
+
+                  {repository.viewerHasStarred ? (
+                    <Unstar repositoryId={repository.id} />
+                  ) : (
+                    <Star repositoryId={repository.id} />
+                  )}
+
                   <span className="black flex ba b--gray bg-light-gray hover-bg-moon-gray pointer br2 pa2 h-100 mh3">
                     <svg
                       className="octicon octicon-repo-forked v-align-text-bottom"
