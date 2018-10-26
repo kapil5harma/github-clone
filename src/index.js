7cdc3e9b220213f90e65074df497bb3c69a4b092
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-26 14:00:08 +0530

    Github new environment variable added -- last one was revoked

--- src/index.js
+++ src/index.js
@@ -25,8 +25,7 @@ const GITHUB_BASE_URL = 'https://api.github.com/graphql';
 const httpLink = new HttpLink({
   uri: GITHUB_BASE_URL,
   headers: {
-    // Authorization: `bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`
-    Authorization: `bearer ${process.env.GITHUB_TOKEN}`
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`
   }
 });
 
